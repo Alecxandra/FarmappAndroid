6186afbfa21afd9153365e401400ff53e3528d98
Edilson Gonzalez <edilson.gonza@unitec.edu>
2015-09-10 13:06:43 -0600

    Hide main manu

--- app/src/main/java/co/wildpro/farmapp/MainActivity.java
+++ app/src/main/java/co/wildpro/farmapp/MainActivity.java
@@ -21,7 +21,7 @@ public class MainActivity extends AppCompatActivity {
     }
 
 
-    @Override
+    /* @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu_main, menu);
@@ -41,7 +41,7 @@ public class MainActivity extends AppCompatActivity {
         }
 
         return super.onOptionsItemSelected(item);
-    }
+    }*/
 
     private void initComponents() {
         mainToolbar = (Toolbar) findViewById(R.id.main_toolbar);
